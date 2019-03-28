861b5a5eda2be7cee7f77b9bd6b37ee935a9aeb7
mehdiboomgmr <40923456+mehdiboomgmr@users.noreply.github.com>
2019-03-28 20:18:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const prefix ="1";
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 
@@ -79,6 +80,26 @@ if (message.content === '!spam') {
           
         }
       }
+});
+
+//say 
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+
+  if (command === "say") { /// اكتب البرفيكس ثم الامر
+          message.delete()
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+  }
+
+
 });
 
 
